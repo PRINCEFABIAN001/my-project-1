584ff4241764c8629ffeabf471a6acdde812ab72
PRINCEFABIAN001 <orukpefabian60@gmail.com>
2026-05-07 11:23:39 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+javascript
+function showmenu() {
+    var menu =
+    document.getElementById("MENULIST"
+    );
+
+    IF(MENU.style.DISPLAY  ===
+    "NONE") 
+     {
+              menu.style.display =
+              "block";
+              alert("menu is now open");
+    } 
+      { 
+        menu.style.display = "none";
+      alert("menu is now hidden!");
+    
+    }
+}
